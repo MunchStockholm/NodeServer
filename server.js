b8b3198b1cf0ee360a6078e0d52dfd1ad096a318
carolinevannebo <58633333+carolinevannebo@users.noreply.github.com>
2023-06-07 13:33:36 +0200

    refactoring

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express, * as bodyParser from "express";
 import router from "./router.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+
 const server = app.listen(3001, () => {
     console.log(`Started on http://localhost:${server.address().port}`);
 });
