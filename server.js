2ed42618b66088b80c3a2915758fbfa7f632109d
Wollando <andiimeri99@gmail.com>
2023-06-08 11:13:35 +0200

    final???

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import csurf from "csurf";
 import router from "./router.js";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 const port = 3001;
@@ -18,12 +19,13 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // Add CSRF protection middleware
-app.use(csurf({ cookie: true }));
+const csrfProtection = csurf({ cookie: true });
+app.use(csrfProtection);
 
 // Generate CSRF token and include it in every response
 app.use((req, res, next) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    next();
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  next();
 });
 
 // Routes should be defined after CSRF middleware
@@ -31,14 +33,17 @@ app.use("", router);
 
 // Error handling for CSRF token validation failure
 app.use((err, req, res, next) => {
-    if (err.code === "EBADCSRFTOKEN") {
-        res.status(403).json({ message: "Invalid CSRF token" });
-    } else {
-        next(err);
-    }
+  if (err.code === "EBADCSRFTOKEN") {
+    res.status(403).json({ message: "Invalid CSRF token" });
+  } else {
+    next(err);
+  }
 });
+
+// Make sure to include the CSRF token in Axios requests
+axios.defaults.headers.common["X-XSRF-TOKEN"] = req.cookies["XSRF-TOKEN"];
 
 // Start the server
 const server = app.listen(port, () => {
-    console.log(`Started on http://localhost:${port}`);
+  console.log(`Started on http://localhost:${port}`);
 });
