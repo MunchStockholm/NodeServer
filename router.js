edee4cee8ec40253868f0ee37b2bca0ac3fa1704
Wollando <andiimeri99@gmail.com>
2023-06-08 14:14:11 +0200

    Revert "hippityHoppity"
    This reverts commit 4a9a09e6b7c8e3348b41aa57a0eeb068c3ec452b.

--- router.js
+++ router.js
@@ -2,7 +2,10 @@ import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import client from './client.js';
+import csrf from 'csrf';
 
+
+const csrfProtection = csrf({ cookie: true })
 const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
@@ -10,87 +13,103 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(morgan('tiny'));
 
-router.get("/", async (req, res) => {
-    try {
-        async function listObjects(client, dbName, collectionName) {
-            await client.connect();
-            const database = client.db(dbName);
-            const collection = database.collection(collectionName);
-            const objects = await collection.find({}).toArray();
-            return objects;
-        }
-
-        const objects = await listObjects(client, "GrafittiWallDB", "ArtWork");
-
-        await client.close();
-
-        res.status(200).send(objects);
-    } catch (e) {
-        console.error(e);
-        if (e instanceof MongoClientError) {
-            res.status(500).send({ message: 'Database Connection Error', error: e.message });
-        } else if (e instanceof MongoParseError) {
-            res.status(400).send({ message: 'Bad Request', error: e.message });
-        } else {
-            res.status(500).send({ message: 'Internal Server Error', error: e.message });
-        }
+router.get("/", csrfProtection, async (req, res) => {
+    const csrfToken = req.cookies["XSRF-TOKEN"];
+    if (!csrfToken || req.body._csrf !== csrfToken) {
+      return res.status(403).json({ message: "Invalid CSRF token" });
     }
-});
-
-router.get("/:id", async (req, res) => {
+  
     try {
-        const collection = "ArtWork";
-        const id = parseInt(req.params.id);
-
+      async function listObjects(client, dbName, collectionName) {
         await client.connect();
-
-        const result = await client
-            .db("GrafittiWallDB")
-            .collection(collection)
-            .findOne({ _id: id });
-
-        await client.close();
-        res.status(200).send(result);
-        
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+        const objects = await collection.find({}).toArray();
+        return objects;
+      }
+  
+      const objects = await listObjects(client, "GrafittiWallDB", "ArtWork");
+  
+      await client.close();
+  
+      res.status(200).send(objects);
     } catch (e) {
-        console.error(e);
-        if (e instanceof MongoClientError) {
-            res.status(500).send({ message: 'Database Connection Error', error: e.message });
-        } else if (e instanceof MongoParseError) {
-            res.status(400).send({ message: 'Bad Request', error: e.message });
-        } else {
-            res.status(500).send({ message: 'Internal Server Error', error: e.message });
-        }
+      console.error(e);
+      if (e instanceof MongoClientError) {
+        res.status(500).send({ message: "Database Connection Error", error: e.message });
+      } else if (e instanceof MongoParseError) {
+        res.status(400).send({ message: "Bad Request", error: e.message });
+      } else {
+        res.status(500).send({ message: "Internal Server Error", error: e.message });
+      }
     }
-});
+  });
+  
 
-router.post("/", async (req, res) => {
+router.get("/:id", csrfProtection, async (req, res) => {
+    const csrfToken = req.cookies["XSRF-TOKEN"];
+    if (!csrfToken || req.query._csrf !== csrfToken) {
+      return res.status(403).json({ message: "Invalid CSRF token" });
+    }
+  
     try {
-        const collection = "ArtWork";
-        const object = req.body;
-
-        await client.connect();
-
-        const result = await client
-            .db("GrafittiWallDB")
-            .collection(collection)
-            .insertOne(object);
-
-        await client.close();
-
-        res.status(200).json(sanitizeResult(result)); 
-
+      const collection = "ArtWork";
+      const id = parseInt(req.params.id);
+  
+      await client.connect();
+  
+      const result = await client
+        .db("GrafittiWallDB")
+        .collection(collection)
+        .findOne({ _id: id });
+  
+      await client.close();
+      res.status(200).send(result);
     } catch (e) {
-        console.error(e);
-        if (e instanceof MongoClientError) {
-            res.status(500).send({ message: 'Database Connection Error', error: e.message });
-        } else if (e instanceof MongoParseError) {
-            res.status(400).send({ message: 'Bad Request', error: e.message });
-        } else {
-            res.status(500).send({ message: 'Internal Server Error', error: e.message });
-        }
+      console.error(e);
+      if (e instanceof MongoClientError) {
+        res.status(500).send({ message: "Database Connection Error", error: e.message });
+      } else if (e instanceof MongoParseError) {
+        res.status(400).send({ message: "Bad Request", error: e.message });
+      } else {
+        res.status(500).send({ message: "Internal Server Error", error: e.message });
+      }
     }
-});
+  });
+  
+
+router.post("/", csrfProtection, async (req, res) => {
+    const csrfToken = req.cookies["XSRF-TOKEN"];
+    if (!csrfToken || req.body._csrf !== csrfToken) {
+      return res.status(403).json({ message: "Invalid CSRF token" });
+    }
+  
+    try {
+      const collection = "ArtWork";
+      const object = req.body;
+  
+      await client.connect();
+  
+      const result = await client
+        .db("GrafittiWallDB")
+        .collection(collection)
+        .insertOne(object);
+  
+      await client.close();
+  
+      res.status(200).json(sanitizeResult(result));
+    } catch (e) {
+      console.error(e);
+      if (e instanceof MongoClientError) {
+        res.status(500).send({ message: "Database Connection Error", error: e.message });
+      } else if (e instanceof MongoParseError) {
+        res.status(400).send({ message: "Bad Request", error: e.message });
+      } else {
+        res.status(500).send({ message: "Internal Server Error", error: e.message });
+      }
+    }
+  });
+  
 
 function sanitizeResult(result) {
     const sanitizedResult = {
@@ -109,60 +128,68 @@ function sanitizeField(value) {
 }
 
 
-router.put("/:id", async (req, res) => {
-    try {
-        const collection = "ArtWork";
-        const id = parseInt(req.params.id);
-        const object = req.body;
-
-        await client.connect();
-
-        const result = await client
-            .db("GrafittiWallDB")
-            .collection(collection)
-            .findOneAndUpdate({ _id: id }, { $set: object });
-
-        await client.close();
-        res.status(200).send(result.value);
-        
-    } catch (e) {
-        console.error(e);
-        if (e instanceof MongoClientError) {
-            res.status(500).send({ message: 'Database Connection Error', error: e.message });
-        } else if (e instanceof MongoParseError) {
-            res.status(400).send({ message: 'Bad Request', error: e.message });
-        } else {
-            res.status(500).send({ message: 'Internal Server Error', error: e.message });
-        }
+router.put("/:id", csrfProtection, async (req, res) => {
+    const csrfToken = req.cookies["XSRF-TOKEN"];
+    if (!csrfToken || req.body._csrf !== csrfToken) {
+      return res.status(403).json({ message: "Invalid CSRF token" });
     }
-});
-
-router.delete("/:id", async (req, res) => {
+  
     try {
-        const collection = "ArtWork";
-        const id = parseInt(req.params.id);
-
-        await client.connect();
-
-        const result = await client
-            .db("GrafittiWallDB")
-            .collection(collection)
-            .findOneAndDelete({ _id: id });
-
-        await client.close();
-
-        res.status(200).send(result.value);
-        
+      const collection = "ArtWork";
+      const id = parseInt(req.params.id);
+      const object = req.body;
+  
+      await client.connect();
+  
+      const result = await client
+        .db("GrafittiWallDB")
+        .collection(collection)
+        .findOneAndUpdate({ _id: id }, { $set: object });
+  
+      await client.close();
+      res.status(200).json(result.value);
     } catch (e) {
-        console.error(e);
-        if (e instanceof MongoClientError) {
-            res.status(500).send({ message: 'Database Connection Error', error: e.message });
-        } else if (e instanceof MongoParseError) {
-            res.status(400).send({ message: 'Bad Request', error: e.message });
-        } else {
-            res.status(500).send({ message: 'Internal Server Error', error: e.message });
-        }
+      console.error(e);
+      if (e instanceof MongoClientError) {
+        res.status(500).send({ message: "Database Connection Error", error: e.message });
+      } else if (e instanceof MongoParseError) {
+        res.status(400).send({ message: "Bad Request", error: e.message });
+      } else {
+        res.status(500).send({ message: "Internal Server Error", error: e.message });
+      }
     }
-});
-
+  });
+  
+  router.delete("/:id", csrfProtection, async (req, res) => {
+    const csrfToken = req.cookies["XSRF-TOKEN"];
+    if (!csrfToken || req.body._csrf !== csrfToken) {
+      return res.status(403).json({ message: "Invalid CSRF token" });
+    }
+  
+    try {
+      const collection = "ArtWork";
+      const id = parseInt(req.params.id);
+  
+      await client.connect();
+  
+      const result = await client
+        .db("GrafittiWallDB")
+        .collection(collection)
+        .findOneAndDelete({ _id: id });
+  
+      await client.close();
+  
+      res.status(200).json(result.value);
+    } catch (e) {
+      console.error(e);
+      if (e instanceof MongoClientError) {
+        res.status(500).send({ message: "Database Connection Error", error: e.message });
+      } else if (e instanceof MongoParseError) {
+        res.status(400).send({ message: "Bad Request", error: e.message });
+      } else {
+        res.status(500).send({ message: "Internal Server Error", error: e.message });
+      }
+    }
+  });
+  
 export default router;
--- server.js
+++ server.js
@@ -1,49 +1,36 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import csrf from 'csurf';
+import client from './client.js';
+import csrf from 'csrf';
 import cookieParser from 'cookie-parser';
 
-const app = express();
+const csrfProtection = csrf({ cookie: true });
 const router = express.Router();
 
 // Enable parsing of JSON bodies
-app.use(bodyParser.json());
+router.use(bodyParser.json());
 
 // Enable parsing of URL-encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({ extended: true }));
 
 // Enable parsing of cookies
-app.use(cookieParser());
+router.use(cookieParser());
 
 // Log requests
-app.use(morgan('tiny'));
+router.use(morgan('tiny'));
 
 // Apply CSRF protection middleware
-app.use(csrf({ cookie: true }));
+router.use(csrfProtection);
 
 // Add CSRF token to every response
-app.use((req, res, next) => {
+router.use((req, res, next) => {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   next();
 });
 
-// RESTful API endpoints
-router.get('/', (req, res) => {
-  // Handle GET request
-});
-
-router.post('/', (req, res) => {
-  // Handle POST request
-});
-
-// ... other routes
-
-// Mount the router on a specific path
-app.use('/', router);
-
 // Handle CSRF token validation failure
-app.use((err, req, res, next) => {
+router.use((err, req, res, next) => {
   if (err.code === 'EBADCSRFTOKEN') {
     res.status(403).json({ message: 'Invalid CSRF token' });
   } else {
@@ -51,8 +38,4 @@ app.use((err, req, res, next) => {
   }
 });
 
-// Start the server
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+export default router;
